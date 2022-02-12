92bf5fd1520e35bed4045bbd9bd6ba0b720fda88
TestAuthor <exaple@gmail.com>
2022-02-12 06:02:44 +0300

    sadasdf

--- Sources/TestPackge/TestPackge.swift
+++ Sources/TestPackge/TestPackge.swift
@@ -13,11 +13,11 @@ public struct TestPackge {
     }
 }
 public class TestDropMenu:UIView {
-    override init(frame: CGRect) {
-           super.init(frame: frame)
-        
-        
-       }
+//    override init(frame: CGRect) {
+//           super.init(frame: frame)
+//        
+//        
+//       }
 
        required init?(coder aDecoder: NSCoder) {
            super.init(coder: aDecoder)
