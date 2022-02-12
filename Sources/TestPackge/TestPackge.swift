4ea7b47d2c2c0c2358f1f8bebc498a88c763da53
TestAuthor <exaple@gmail.com>
2022-02-12 06:07:47 +0300

    ssss

--- Sources/TestPackge/TestPackge.swift
+++ Sources/TestPackge/TestPackge.swift
@@ -19,7 +19,7 @@ public class TestDropMenu:UIView {
        required init?(coder aDecoder: NSCoder) {
            super.init(coder: aDecoder)
            self.backgroundColor = .red
-           var button = UIButton(frame: CGRect(x: self.frame.minX / 2, y: 40, width: self.frame.minX / 2, height: 45))
+           var button = UIButton(frame: CGRect(x: self.frame.minX / 2, y: 40, width: self.frame.minX / 1.5, height: 45))
            button.setTitleColor(.black, for: .normal)
            button.setTitle("Selected", for: .normal)
            self.addSubview(button)
